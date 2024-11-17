99cbd7d017a04d623dee638446dfbc1a00fa7839
Crypto-Neko <objective.axioma@gmail.com>
2024-11-17 17:26:55 -0500

    First commit: setting up .py files and general program structure.

---
+++ kyber_kem.py
@@ -0,0 +1,11 @@
+from sage.all import *
+import random as rand
+
+def kyber_keygen():
+    pass
+
+def kyber_encaps(pk, m):
+    pass
+
+def kyber_decaps(sk, c):
+    pass
---
+++ kyber_pke.py
@@ -0,0 +1,11 @@
+from sage.all import *
+import random as rand
+
+def kyber_keygen():
+    pass
+
+def kyber_encrypt(pk, m):
+    pass
+
+def kyber_decrypt(sk, c):
+    pass
