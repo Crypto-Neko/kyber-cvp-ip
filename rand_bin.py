31003b571c4fd4bf8b64ea906ff39b5f43cb3de9
Felyne Quaternyan <crypto.neko.me@gmail.com>
2024-11-20 17:02:52 -0500

    Saving Kyber implementation for another repo and focusing on integer programming to compute lattice points.

--- rand_bin.py
+++
@@ -1,9 +0,0 @@
-import random as rand
-
-# Generate a random binary string--used in keygen
-def gen_rand_bin(n):
-    binary = ''
-    for i in range (n):
-        binary.append(str(rand.randint(0, 1)))
-    
-    return binary
